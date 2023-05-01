3fe011af3bf8ef511784475da8e75423c6e24990
tudoukuaipao123 <zhangxing114@126.com>
2023-05-01 13:12:06 +0800

    1.添加遍历fiber俩表的逻辑2.处理fiber在链表中指针child,return,sibling的逻辑

--- src/zhangReact/index.js
+++ src/zhangReact/index.js
@@ -2,18 +2,71 @@ import ReactElement from './ReactElement.js';
 let nextFiberReconcileWork = null; // 下一个需要调度的的fiber节点
 let wipRoot = null; // 渲染时的根fiber节点
 
-// 处理当前节点
-function reconcile(fiber) {
+// 根据fiber生成dom
+function createDom(fiber) {
 
+}
+
+// 处理fiber和children之间的关系,添加child和sibling的指向
+function reconcileChildren(wipFiber, elements) {
+  // 关于链表的处理流程
+  let index = 0;
+  let preSibling = null;
+
+  while(index<elements.length) {
+    const element = elements[index];
+    let newFiber = {
+      dom: null,
+      props: element.props,
+      type: element.type,
+      return: wipFiber,
+      effectTag: "PLACEMENT", // 因为这里只处理新增，就写死成PLACEMENT
+    }
+
+    // 若是第一个则直接设置父fiber的child，若是相邻的则通过sibling组成链表
+    if (index == 0) {
+      wipFiber.child = newFiber;
+    } else {
+      // element.sibling = preSibling;
+      preSibling.sibling = newFiber;
+    }
+    preSibling = newFiber;
+    // 注意 这里尽量不要再原来的element上修改，而是新建一个fiber，引用老的props和type
+    // preSibling = element; <--所以左侧这种写法错误
+    index++;
+  }
+
+}
+
+// 处理当前节点，做两件事
+function reconcile(fiber) {
+  // 1.本fiber节点如果没有dom，则生成dom，挂在当前节点下
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  // 2.处理本fiber的children
+  reconcileChildren(fiber, fiber.props.children);
 }
 
 // 该方法做了两个操作，1.处理当前节点 2.定位下一个节点
 function performNextWork(fiber) {
+  //1.处理当前节点
   reconcile(fiber);
+  //2.定位下一个节点
+  console.log('fiber', fiber);
   if (fiber.child) {
     return fiber.child;
   }
-
+  let nextFiber = fiber;
+  // while是确保能一直找到没有sibling也没有父fiber为止，精妙的双层函数循环遍历！！！
+  while(nextFiber) {
+    // 如果是有兄弟值，则直接返回，下次外面的while方法会把兄弟值再赋进来，达到里外函数配合遍历链表
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    // 如果没有兄弟值，则返回上一层，如果还没有则继续往上，
+    nextFiber = nextFiber.return;
+  }
 
 }
 
